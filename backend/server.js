2e7ff1276868227a429197aa051649b75fdce1f3
PatrickChewing8 <84030379+PatrickChewing8@users.noreply.github.com>
2022-06-20 23:15:10 -0400

    Second commit

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,25 @@
-console.log('Server...');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+const colors = require('colors');
+
+// Connect to database
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+	res.status(200).json({ message: 'Welcome to the Support Desk API' });
+});
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server stated on port ${PORT}`));
