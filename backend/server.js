a457effa7c3e7f8961cd97b46efc1589b78e0fa9
PatrickChewing8 <84030379+PatrickChewing8@users.noreply.github.com>
2022-06-23 09:29:49 -0400

    Prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 const { errorHandler } = require('./middleware/errorMiddleware');
@@ -20,6 +21,21 @@ app.get('/', (req, res) => {
 // Routes
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/tickets', require('./routes/ticketRoutes'));
+
+// Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+	// Set build folder as static
+	app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+	app.get('*', (req, res) =>
+		respond.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'),
+	);
+} else
+	[
+		app.get('/', (req, res) => {
+			res.status(200).json({ message: 'Welcome to the Support Desk API' });
+		}),
+	];
 
 app.use(errorHandler);
 
--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
 import ticketReducer from '../features/tickets/ticketSlice';
+import noteSlice from '../features/notes/noteSlice';
+import noteReducer from '../features/notes/noteSlice';
 
 export const store = configureStore({
 	reducer: {
 		auth: authReducer,
 		tickets: ticketReducer,
+		notes: noteReducer,
 	},
 });
