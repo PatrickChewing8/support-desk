43f4801c96a89d9a6ea9150d71440e61fe5273c1
PatrickChewing8 <84030379+PatrickChewing8@users.noreply.github.com>
2022-06-22 22:52:03 -0400

    Third Commit

---
+++ frontend/src/components/BackButton.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FaArrowCircleLeft } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+const BackButton = ({ url }) => {
+	return (
+		<Link className='btn btn-reverse btn-back' to={url}>
+			<FaArrowCircleLeft />
+			Back
+		</Link>
+	);
+};
+
+export default BackButton;
